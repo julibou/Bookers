eb8676957e30a33a9a54cc57841524515fe131f8
julibou777 <julibou777@gmail.com>
2021-07-20 10:40:44 +0000

    book create

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+
+  end
+  def show
+  end
+
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/new'
-  get 'blogs/edit'
+  resources :blogs
+  resources :homes
+  resources :books
+  root :to => "homes#top"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
